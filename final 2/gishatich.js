b4838dee39d1c3cc6922f09da50a5ceec724cb4e
Elennn12 <elen.avanesyan2.y@tumo.org>
2023-05-06 14:26:24 +0400

    prog2 final

---
+++ final 2/gishatich.js
@@ -0,0 +1,117 @@
+class Gishatich {
+
+    constructor(x, y, index) {
+        this.x = x;
+        this.y = y;
+        this.index = index;
+        this.energy = 15;
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+
+
+    }
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+    chooseCell(character) {
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+
+                if (matrix[y][x] == character) {
+                    found.push(this.directions[i]);
+                }
+            }
+
+        }
+        return found;
+
+    }
+
+    mul() {
+        var newCell = random(this.chooseCell(0));
+        if (newCell) {
+            var newGishatich = new Gishatich(newCell[0], newCell[1], this.index);
+            GishatichArr.push(newGishatich);
+            matrix[newCell[1]][newCell[0]] = 3;
+            this.energy = 30
+        }
+    }
+    move() {
+
+        this.energy--;
+
+        console.log(this.energy);
+        let emptyCells = this.chooseCell(0)
+        let newCell = random(emptyCells)
+        if (newCell) {
+
+            let newX = newCell[0]
+            let newY = newCell[1]
+            matrix[this.y][this.x] = 0
+            matrix[newY][newX] = 3
+            this.x = newX
+            this.y = newY
+        }
+        if (this.energy <= 0) {
+            this.die()
+        }
+    }
+
+    eat() {
+
+        let foods = this.chooseCell(2)
+        let food = random(foods)
+        if (food) {
+            this.energy++
+            matrix[this.y][this.x] = 0
+            let newX = food[0]
+            let newY = food[1]
+            matrix[food[1]][food[0]] = 3
+            this.x = newX
+            this.y = newY
+            for (var i in GishatichArr) {
+                if (newX == GishatichArr[i].x && newY == GishatichArr[i].y) {
+                    GishatichArr.splice(i, 1);
+                    break;
+                }
+            }
+            if (this.energy >= 5 ) {
+                this.mul()
+            }
+        }
+        else {
+            this.move()
+        }
+    }
+    die() {
+        console.log("merav");
+
+        matrix[this.y][this.x] = 0
+        for (var i in GishatichArr) {
+            if (this.x == GishatichArr[i].x && this.y == GishatichArr[i].y) {
+                GishatichArr.splice(i, 1);
+                break;
+            }
+        }
+    }
+}
