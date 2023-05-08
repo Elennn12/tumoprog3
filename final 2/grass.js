66411d94bda6dac1931ec5fb6af88431cba9fd9c
arame190 <elen.avanesyan2.y@tumo.org>
2023-05-08 20:28:25 +0400

    inheritance

--- final 2/grass.js
+++ final 2/grass.js
@@ -1,4 +1,4 @@
-class Grass {
+/*class Grass {
     constructor(x, y, index) {
         this.x = x;
         this.y = y;
@@ -47,4 +47,29 @@ class Grass {
 
     }
 
+}*/
+
+class Grass extends LivingCreature {
+
+    mul() {
+
+        this.multiply++;
+
+        var newCell = random(this.chooseCell(0));
+
+        if (this.multiply >= 8 && newCell) {
+
+            var newGrass = new Grass(newCell[0], newCell[1], this.index);
+
+            grassArr.push(newGrass);
+
+            matrix[newCell[1]][newCell[0]] = this.index;
+
+            this.multiply = 0;
+
+        }
+
+    }
+
 }
+
--- final 2/grassEater.js
+++ final 2/grassEater.js
@@ -1,4 +1,4 @@
-class GrassEater {
+/*class GrassEater {
 
     constructor(x, y, index) {
         this.x = x;
@@ -114,4 +114,49 @@ class GrassEater {
             }
         }
     }
-}
+}*/
+
+class GrassEater extends LivingCreature {
+
+    constructor(x, y, index){
+    
+    super(x, y, index);
+    this.energy = 8;
+    
+    }
+    
+    getNewCoordinates() {
+    
+    this.directions = [
+    
+    [this.x - 1, this.y - 1],
+    
+    [this.x, this.y - 1],
+    
+    [this.x + 1, this.y - 1],
+    
+    [this.x - 1, this.y],
+    
+    [this.x + 1, this.y],
+    
+    [this.x - 1, this.y + 1],
+    
+    [this.x, this.y + 1],
+    
+    [this.x + 1, this.y + 1]
+    
+    ];
+    
+    }
+    
+    chooseCell(character) {
+    
+    this.getNewCoordinates();
+    
+    return super.chooseCell(character);
+    
+    }
+    
+    // eat, mul, move, die
+    
+    }
