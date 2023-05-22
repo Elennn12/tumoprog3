38c81f7893094ea859be75b69e43f167a12b4f99
Elennn12 <elen.avanesyan2.y@tumo.org>
2023-05-22 20:27:14 +0400

    lesson 7

--- client/script2.js
+++ client/script2.js
@@ -1,4 +1,41 @@
+var socket = io()
+socket.on("my_matrix", my_draw)
 function setup() {
     frameRate(10);
-    createCanvas(400,400);
-    background('#acacac');} //client
+    createCanvas(400, 400);
+    background('#acacac');
+} //client
+
+function my_draw(matrix) {
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+
+            if (matrix[y][x] == 1) {
+                fill(29, 105, 13);
+            }
+            else if (matrix[y][x] == 0) {
+                fill(115, 67, 3);
+            }
+            else if (matrix[y][x] == 2) {
+                fill(120, 18, 23);
+            }
+            else if (matrix[y][x] == 3) {
+                fill(250, 163, 12);
+            }
+            else if (matrix[y][x] == 4) {
+                fill(13, 61, 94);
+            }
+            else if (matrix[y][x] == 5) {
+                fill(148, 12, 114);
+            }
+
+            rect(x * 10, y * 10, 10, 10);
+
+            //textSize (30)
+            //fill("blue")
+            //text(x + " " + y, x * side + side / 2, y * side + side / 2)
+
+        }
+    }
+
+}
--- server/server.js
+++ server/server.js
@@ -9,8 +9,10 @@ app.get("/", function(req, res){
 res.redirect("index.html");
 
 });
+var server = require('http').createServer(app);
 
-app.listen(3000, function(){
+var io = require('socket.io')(server);
+server.listen(3000, function(){
 
 console.log("Game is running on port 3000");
 
@@ -21,6 +23,12 @@ var Gishatich = require("./gishatich")
 var Grass = require("./grass")
 var Terrorist = require("./terrorist")
 var Hunter = require("./hunter")
+
+ grassArr = []
+ grassEaterArr = []
+ GishatichArr = []
+ hunterArr = []
+ terroristArr =[]
 
 matrix = []
 function getRandInt(min,max){
@@ -35,5 +43,65 @@ for (let i = 0; i<100; i++){
     }
 }
 }
+
+
+function createobject(){
+    for (let y = 0; y < matrix.length; y++) {
+        for (let x = 0; x < matrix[y].length; x++) {
+            if (matrix[y][x] == 1) {
+                let grass = new Grass(x, y, 1)
+                grassArr.push(grass)
+            }
+            else if (matrix[y][x] == 2) {
+                let grassEater = new GrassEater(x, y, 2)
+                grassEaterArr.push(grassEater)
+            }
+            else if (matrix[y][x] == 3) {
+                let gishatich = new Gishatich(x, y, 3)
+                GishatichArr.push(gishatich)
+            }
+            else if (matrix[y][x] == 4) {
+                let hunter = new Hunter (x, y, 4)
+                hunterArr.push(hunter)
+            }
+            else if (matrix[y][x] == 5) {
+                let terrorist = new Terrorist (x, y, 5)
+                terroristArr.push(terrorist)
+            }
+
+        }
+
+    }
+}
+function game(){
+    /*for (var i in grassArr) {
+        grassArr[i].mul();
+    }
+
+    for (var i in grassEaterArr) {
+        grassEaterArr[i].eat();
+    }
+    for (var i in GishatichArr) {
+        GishatichArr[i].eat();
+    }
+    for (var i in hunterArr) {
+        hunterArr[i].eat();
+    }
+    for (var i in terroristArr) {
+        terroristArr[i].eat();
+    }*/
+
+  io.sockets.emit("my_matrix", matrix);
+}
+
+
 generateMatrix()
-console.log(matrix)
+createobject()
+setInterval(game, 1000)
+
+io.on('connection', function (socket) { 
+    socket.emit("my_matrix", matrix);
+
+})
+
+
